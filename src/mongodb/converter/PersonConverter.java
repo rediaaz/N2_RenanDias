16cb15465a07976d16b2b8db5a1c415bee7a39ae
Renan <renan_dias@outlook.com>
2018-11-29 23:43:38 -0200

    .

---
+++ src/mongodb/converter/PersonConverter.java
@@ -0,0 +1,30 @@
+package mongodb.converter;
+
+import org.bson.types.ObjectId;
+
+import mongodb.model.Person;
+import com.mongodb.BasicDBObjectBuilder;
+import com.mongodb.DBObject;
+
+public class PersonConverter {
+
+	public static DBObject toDBObject(Person p) {
+
+		BasicDBObjectBuilder builder = BasicDBObjectBuilder.start()
+				.append("name", p.getName()).append("country", p.getCountry());
+		if (p.getId() != null)
+			builder = builder.append("_id", new ObjectId(p.getId()));
+		return builder.get();
+	}
+
+	public static Person toPerson(DBObject doc) {
+		Person p = new Person();
+		p.setName((String) doc.get("name"));
+		p.setCountry((String) doc.get("country"));
+		ObjectId id = (ObjectId) doc.get("_id");
+		p.setId(id.toString());
+		return p;
+
+	}
+	
+}
